7e170967161607a69ffc1dab31ced230f37b92d7
Xaldersoul <benjamin.g.guerra@hotmail.com>
2021-03-27 13:29:41 -0300

    node

---
+++ index.js
@@ -0,0 +1,24 @@
+const { EAFNOSUPPORT } = require("constants")
+const http = require("http")
+
+const server = http.createServer((request, response) => {
+    console.log(request.url)
+
+    switch (request.url) {
+
+        case "/":
+            response.writeHead(200, { "Content-type": "application/json" })
+            // response.statusMessage("OK")
+            response.end(JSON.stringify("Hola mundo"))
+            break;
+        default:
+            response.writeHead(404);
+            response.end("nada encontrado")
+
+    }
+
+})
+
+server.listen(3000, () => {
+    console.log("servidor operativo en puerto 3000")
+})
